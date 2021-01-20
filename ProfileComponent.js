fd42e5d306f39484a87a43247ff7f26eb4c62fb9
ghada-web <73882275+ghada-web@users.noreply.github.com>
2021-01-21 00:12:34 +0100

    Update ProfileComponent.js

--- ProfileComponent.js
+++ ProfileComponent.js
@@ -5,7 +5,7 @@ const ProfileComponent=(props)=>{
     const handleName= props.FullName ?(alert(props.FullName)):(alert('unknowing'));
     
     return(
-    <div style={{ fontFamily:'sans-serif',fontSize:'bold ', backgroundColor:'LightBlue', width:'500px', padding:'10px',margin:'10px auto'}}>
+    <div style={{ fontFamily:'sans-serif', backgroundColor:'LightBlue', width:'500px', padding:'10px',margin:'10px auto'}}>
          {handleName}
         <p>{ props.children}</p>
          <p> Name : {props.FullName}</p>
